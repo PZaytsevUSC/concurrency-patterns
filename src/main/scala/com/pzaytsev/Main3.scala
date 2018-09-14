61ed72fea7a5a845f943fc4a9780a32b4e092a7d
PZaytsevUSC <pzaytsev@usc.edu>
2018-09-14 04:15:26 -0700

    Promises

---
+++ src/main/scala/com/pzaytsev/Main3.scala
@@ -0,0 +1,33 @@
+package com.pzaytsev
+
+import scalaz.zio._
+import scalaz.zio.console._
+
+/** Simple promise example
+  *
+  *
+  */
+object Main3 extends RTS {
+
+  def main(args: Array[String]): Unit = {
+    def sideEffectingAssigningValueProcess(
+        p: Promise[Unit, String]): IO[Nothing, Boolean] = {
+      Thread.sleep(5000)
+
+      p.complete("Precious answer")
+    }
+
+    val program = for {
+      p <- Promise.make[Unit, String]
+      succeeded <- sideEffectingAssigningValueProcess(p)
+      result <- succeeded match {
+        case true  => p.get
+        case false => IO.point("empty")
+      }
+      _ <- putStrLn(result)
+    } yield ()
+
+    unsafeRun(program)
+  }
+
+}
